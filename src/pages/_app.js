469a1e41fb7c3942eee291fc972e52370fcb4dae
Marcello Silvério <smarcelloc@gmail.com>
2020-12-27 01:49:45 -0300

    Configurado module-alias para nextjs

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
-import { ThemeProvider } from 'styled-components'
-import theme from './../styles/theme';
-import GlobalStyle from './../styles/global';
+import { ThemeProvider } from 'styled-components';
+import theme from '@styles/theme';
+import GlobalStyle from '@styles/global';
 
 export default function App({ Component, pageProps }) {
   return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import styled from 'styled-components';
-
 const Title = styled.h1`
   color: ${(props) => props.theme.colors.primary}
 `
 
 function Index() {
-
   return <Title>Olá Mundo</Title>
 }
 
