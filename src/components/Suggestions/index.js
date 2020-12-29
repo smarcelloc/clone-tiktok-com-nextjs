2e90fd69bbbdb3ef76077a58d19b1f0a72690827
Marcello Silvério <smarcelloc@gmail.com>
2020-12-29 14:01:56 -0300

    Implementado recommendCard

--- src/components/Suggestions/index.js
+++ src/components/Suggestions/index.js
@@ -1,3 +1,64 @@
+import { FacebookIcon, PinterestIcon, TwitterIcon, ArrowIcon } from "@components/Icons";
+import {
+  Container,
+  IconsContainer,
+  BoxContainer,
+  BoxHeader,
+  ButtonContainer,
+  Item,
+  ItemContainer,
+  DownloadImage,
+} from './styles';
+
+import { User } from '@components/User';
+import Button from '@components/Button';
+import { RecommendCard } from '@components/RecommendCard';
+
+
+import { people } from "./people";
+import { items } from "./items";
+
 export function Suggestions() {
-  return <div></div>;
+  return (
+    <Container>
+      <IconsContainer>
+        <FacebookIcon />
+        <PinterestIcon />
+        <TwitterIcon />
+      </IconsContainer>
+      <BoxContainer>
+        <BoxHeader>
+          <span>Contas sugeridas</span>
+        </BoxHeader>
+        <ItemContainer>
+          {people.map((person, index) => (
+            <Item>
+              <User key={index} user={person}></User>
+              <ButtonContainer>
+                <Button fontSize={14} outlined>
+                  Seguir
+                </Button>
+              </ButtonContainer>
+            </Item>
+          ))}
+        </ItemContainer>
+      </BoxContainer>
+      <BoxContainer>
+        <BoxHeader>
+          <span>Descobrir</span>
+        </BoxHeader>
+        <ItemContainer>
+          {items.map((item, index) => (
+            <Item>
+              <RecommendCard key={index} recommend={item}></RecommendCard>
+              <ArrowIcon />
+            </Item>
+          ))}
+        </ItemContainer>
+      </BoxContainer>
+      <DownloadImage src='/images/appstore.png'></DownloadImage>
+      <DownloadImage src='/images/playstore.png'></DownloadImage>
+      <DownloadImage src='/images/amazon.png'></DownloadImage>
+    </Container>
+  );
 }
