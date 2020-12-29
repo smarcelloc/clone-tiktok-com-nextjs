d6fcabb484a9f4f2f63432b23054777bae03a368
Marcello Silvério <smarcelloc@gmail.com>
2020-12-29 02:31:52 -0300

    Configurado o styled-components e module-alias

---
+++ src/pages/_app.js
@@ -0,0 +1,14 @@
+import { ThemeProvider } from 'styled-components';
+import theme from '@styles/theme';
+import GlobalStyle from '@styles/global';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
